2013bc8fdd0aec7a0b960adb2a17c68ac4189f63
Eric Escamilla <eric2esca@gmail.com>
2019-10-11 14:15:19 -0500

    testing heroku deployment

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ import './App.css';
 
 class App extends Component{
   render(){
-    let name = 'Eric';
-    let green = '#FF1212';
-    let headerStyle = {color: green, 'font-size': '50px'};
     return(
       <div className='App'>
         <h1>House Tunes v0.0.1</h1>
